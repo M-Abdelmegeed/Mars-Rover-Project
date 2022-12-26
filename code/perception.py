5b075fa57dcae01a50bbca13c182a00db378cb39
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2022-12-26 23:19:04 +0200

    update the threshold of the obstacle

--- code/perception.py
+++ code/perception.py
@@ -12,8 +12,11 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
     above_thresh = (img[:,:,0] > rgb_thresh[0]) \
+                & (img[:,:,0] <=255) \
                 & (img[:,:,1] > rgb_thresh[1]) \
-                & (img[:,:,2] > rgb_thresh[2])
+                & (img[:,:,1] <= 255) \
+                & (img[:,:,2] > rgb_thresh[2]) \
+                & (img[:,:,2] <=255)
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
     # Return the binary image
