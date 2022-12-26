b656f5db25858779a8719b66a6ac8cc147bc3991
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2022-12-27 00:23:06 +0200

    test fedality

--- code/decision.py
+++ code/decision.py
@@ -50,7 +50,7 @@ def decision_step(Rover):
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = -15 # Could be more clever here about which way to turn
+                    Rover.steer = -11 # Could be more clever here about which way to turn
                 # If we're stopped but see sufficient navigable terrain in front then go!
                 if len(Rover.nav_angles) >= Rover.go_forward:
                     # Set throttle back to stored value
