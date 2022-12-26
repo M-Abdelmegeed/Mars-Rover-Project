ddac751d6f5637e7fc7019008f1b7bdc28aa408d
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2022-12-27 00:12:48 +0200

    make the rover go collect rock

--- code/perception.py
+++ code/perception.py
@@ -202,6 +202,7 @@ def perception_step(Rover):
         Rover.samp_dists = rock_dist
         Rover.samp_angles = rock_ang
 
+        Rover.near_sample=1
         Rover.worldmap[rock_ycen,rock_xcen,1]=255
         Rover.vision_image[:,:,1]=rock_map*255
 
