d2085d577331727069d5b7bb7a595281f7d04a4a
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2022-12-28 11:06:15 +0200

    testing

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -53,15 +53,15 @@ class RoverState():
         self.nav_dists = None # Distances of navigable terrain pixels
         self.ground_truth = ground_truth_3d # Ground truth worldmap
         self.mode = 'forward' # Current mode (can be forward or stop)
-        self.throttle_set = 0.1 # Throttle setting when accelerating
-        self.brake_set = 5 # Brake setting when braking
+        self.throttle_set = 0.1# Throttle setting when accelerating
+        self.brake_set = 5# Brake setting when braking
         # The stop_forward and go_forward fields below represent total count
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
         self.stop_forward = 50 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 1.0 # Maximum velocity (meters/second)
+        self.max_vel = 1.0# Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
