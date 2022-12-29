e1c62d9322df3c375ca5f7e2ffc2916aa167f468
Kareem Azab <kaazab35@gmail.com>
2022-12-29 13:51:42 +0200

    modify perception

--- code/perception.py
+++ code/perception.py
@@ -243,6 +243,8 @@ def perception_step(Rover):
         #print("Rover.sample_pos: ",Rover.samples_pos)
         #print("Rover.samples_pos test: ", rock_x_world, rock_y_world)
 
+        Rover.samples_digits=rock_dist
+        Rover.samples_rock_ang
         
        
         Rover.worldmap[rock_ycen,rock_xcen,1]=255
