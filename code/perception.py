1b1da47a836421504c5236cf808abc0c1142c834
Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>
2023-01-02 13:24:12 +0200

    Modified perception.py

--- code/perception.py
+++ code/perception.py
@@ -206,7 +206,7 @@ def perception_step(Rover):
 
 
 
-        # Update world map if we are not tilted more than 0.5 deg
+        # Update world map if we are not tilted more than 0.5 degrees
     if (Rover.pitch < 1 or Rover.pitch > 359) and (Rover.roll < 1 or Rover.roll > 359) and Rover.mode != 'found' :
 
         Rover.worldmap[y_world, x_world, 2] += 10 # Coloring the blue channel for the navigable road
