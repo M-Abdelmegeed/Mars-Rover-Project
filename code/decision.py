1919af8c7e9a9ae61256a7b94c893a4a760313b1
Kareem Azab <kaazab35@gmail.com>
2023-01-04 18:29:40 +0200

    Made some modifications

--- code/decision.py
+++ code/decision.py
@@ -84,7 +84,7 @@ def decision_step(Rover):
                     Rover.brake = 0
                     # Set steer to mean angle
                     Rover.steer = np.clip(
-                        np.mean((Rover.nav_angles * 180/np.pi)*0.9), -15, 15)
+                        np.mean((Rover.nav_angles * 180/np.pi)), -15, 15)*0.9
                     Rover.mode = 'forward'
 
         elif Rover.mode == 'found':
@@ -105,7 +105,7 @@ def decision_step(Rover):
                     Rover.brake = 0
                     Rover.throttle = 0.2
                     Rover.steer = np.clip(
-                        np.mean((Rover.samples_angles * 180/np.pi)*0.9), -15, 15)
+                        np.mean((Rover.samples_angles * 180/np.pi)), -15, 15)*0.9
 
     # Just to make the rover do something
     # even if no modifications have been made to the code
